efb635b96d97fca8579a57c0e543b3760d9bcf19
노은호 <neh1104@hanyang.ac.kr>
2026-05-22 19:30:44 +0900

    [Passed] 시험 통과 여부 확인하기 2 (49ms, 16MB)

--- trail1/시험 통과 여부 확인하기 2/verify-test-passed-2.py
+++ trail1/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -4,8 +4,8 @@ arr = [list(map(int, input().split())) for i in range(n)]
 ls = 0
 for i in arr:
     x = 0
-    for j in range(4):
-        x += i[j]
+    for j in range(4):     #it doesn't work effectly! Just use sum(i), do ot calculate one by one
+        x += i[j]   
     if x / 4 >= 60:
         ls += 1
         print('pass')
