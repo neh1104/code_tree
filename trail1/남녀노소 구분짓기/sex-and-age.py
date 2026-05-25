231951a68fd9157bb6dc266c55fad6d62eaa5e1e
노은호 <neh1104@hanyang.ac.kr>
2026-05-25 22:20:44 +0900

    [Passed] 남녀노소 구분짓기 (50ms, 16MB)

---
+++ trail1/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,13 @@
+male = int(input())
+age = int(input())
+
+if male == 0:
+    if age >= 19:
+        print('MAN')
+    else:
+        print('BOY')
+else:
+    if age >= 19:
+        print('WOMAN')
+    else:
+        print('GIRL')
