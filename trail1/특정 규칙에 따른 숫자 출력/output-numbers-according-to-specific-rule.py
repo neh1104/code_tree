4503a0d98afaf69ecaf572da7d5743eb1faf8af7
노은호 <neh1104@hanyang.ac.kr>
2026-05-31 13:06:24 +0900

    [Passed] 특정 규칙에 따른 숫자 출력 (54ms, 16MB)

---
+++ trail1/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+cnt = 1
+for i in range(n):
+    for _ in range(i):
+        print(' ', end = ' ')
+    for j in range(n-i):
+        print(cnt, end = ' ')
+        cnt += 1
+        if cnt > 9:
+            cnt = 1
+    print()
