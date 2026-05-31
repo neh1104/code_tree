442886b2a625f46555c33a3ea15238401481e3ec
노은호 <neh1104@hanyang.ac.kr>
2026-05-31 16:56:18 +0900

    [Passed] 특정 조건에 맞게 출력하기 (53ms, 16MB)

---
+++ trail1/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+
+
+for i in arr:
+    if i == 0:
+        break
+    
+    if i % 2 == 0:
+        print(i // 2, end = ' ')
+    else:
+        print(i + 3, end = ' ')
