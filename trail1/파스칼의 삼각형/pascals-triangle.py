642c44bb4fd2a227c3592845682cb86bcd2aa72c
노은호 <neh1104@hanyang.ac.kr>
2026-06-03 12:34:52 +0900

    [Passed] 파스칼의 삼각형 (47ms, 16MB)

---
+++ trail1/파스칼의 삼각형/pascals-triangle.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+a = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+a[0][0] = 1
+for i in range(1, n):
+    a[i][0] = 1
+    for j in range(1, i+1):
+        a[i][j] = a[i-1][j-1] + a[i-1][j]
+        
+for i in range(n):
+    for j in range(i+1):
+        print(a[i][j], end = ' ')
+    print()
