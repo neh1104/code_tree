5bc62e6eb669c5597583a1135e85fb8fdc46068c
노은호 <neh1104@hanyang.ac.kr>
2026-06-07 00:21:12 +0900

    [Passed] 1이 되는 순간까지 (48ms, 16MB)

---
+++ trail2/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+# Please write your code here.
+
+def f(n):
+    if n == 1:
+        return 0
+    
+    if n % 2 == 0:
+        return f(n // 2) + 1
+    else:
+        return f(n//3) + 1
+
+print(f(N))
