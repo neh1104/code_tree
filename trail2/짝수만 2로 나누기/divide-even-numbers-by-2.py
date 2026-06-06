f1868f35dc1974092e7de7aa4a1a64096d2c74d4
노은호 <neh1104@hanyang.ac.kr>
2026-06-06 16:13:40 +0900

    [Passed] 짝수만 2로 나누기 (67ms, 16MB)

---
+++ trail2/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+def re_2(arr):
+    for i in range(n):
+        if arr[i] % 2 == 0:
+            arr[i] //= 2
+
+re_2(arr)
+print(*arr)
