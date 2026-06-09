2305f54d240fc113556828c96bfad12ed2286e5a
노은호 <neh1104@hanyang.ac.kr>
2026-06-09 23:25:07 +0900

    [Passed] 줄 세우기 2 (61ms, 16MB)

---
+++ trail2/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+students = [
+    (h, w, i + 1)
+    for i, (h, w) in enumerate([tuple(map(int, input().split())) for _ in range(n)])
+]
+
+# Please write your code here.
+
+students.sort(key = lambda x: (x[0], -x[1]))
+
+for x in students:
+    print(x[0], x[1], x[2])
