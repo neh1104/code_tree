858c4dcbac6e5c928617614e9d84967132e00ff1
노은호 <neh1104@hanyang.ac.kr>
2026-06-09 15:15:19 +0900

    [Passed] 재귀함수를 이용한 최소공배수 (53ms, 16MB)

---
+++ trail2/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,43 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+def insu(n):
+    i = 2; ls = []
+    while True:
+        if n % i == 0:
+            ls.append(i)
+            n = n // i
+        else:
+            i += 1
+        if n == 1:
+            ls.append(1)
+            break
+    return ls
+
+def cha(a, b):
+    for i in b:
+        if i in a:
+            a.remove(i)
+    return a
+
+def wornl(n):
+    if n == 1:
+        return arr[0]
+
+    ls = cha(insu(wornl(n-1)), insu(arr[n-1]))+insu(arr[n-1])
+    prod = 1 #이게 재귀보다 앞에 있어야 프로그램이 돌아감
+    for i in ls:
+        prod *= i
+    return prod
+
+print(wornl(n))
+
+#print(insu(5), insu(10), insu(2), insu(6))
+#print(cha(insu(5), insu(10)), cha(insu(10), insu(2)))
+#prod = 1
+#for i in cha([1, 2, 2, 3], (1, 3, 7))+[1, 3, 7]:
+#   prod *= i
+
+#print(prod)
