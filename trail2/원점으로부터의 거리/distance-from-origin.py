fd9e1e42c7017bc2c6b578b3863f8ec14f46f0e0
노은호 <neh1104@hanyang.ac.kr>
2026-06-09 23:03:08 +0900

    [Passed] 원점으로부터의 거리 (74ms, 17MB)

---
+++ trail2/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,9 @@
+n = int(input())
+points = [(int(i), tuple(map(int, input().split()))) for i in range(n)]
+
+# Please write your code here.
+
+points.sort(key = lambda x: abs(x[1][0])+abs(x[1][1]))
+
+for i in points:
+    print(i[0]+1)
