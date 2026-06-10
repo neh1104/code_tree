db40c4ffa56b44c1f8505f3447c79a640f422b5a
노은호 <neh1104@hanyang.ac.kr>
2026-06-10 22:17:03 +0900

    [Passed] 최대로 겹치는 지점 (55ms, 17MB)

--- trail2/최대로 겹치는 지점/maximum-overlapped-points.py
+++ trail2/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -3,10 +3,10 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 
-ls = [0]*(101)
+ls = [0]*(100)
 
 for tup in segments:
-    a = tup[0]; b = tup[1]+1
+    a = tup[0]-1; b = tup[1]
     for i in range(a, b):
         ls[i] += 1
 print(max(ls))
