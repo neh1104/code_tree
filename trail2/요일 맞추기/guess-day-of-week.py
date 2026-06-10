4fb1e7bcb3b27f3f2cb5addd5dc3c5c36280ce32
노은호 <neh1104@hanyang.ac.kr>
2026-06-10 19:06:31 +0900

    [Passed] 요일 맞추기 (49ms, 16MB)

---
+++ trail2/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,24 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def f(m, d):
+    cnt = 0; dd = 1; mm = 1
+    while True:
+        if d == dd and m == mm:
+            break
+        if dd == month[mm-1]:
+            dd = 0
+            mm += 1
+        dd += 1
+        cnt += 1
+    
+    return cnt
+
+#print(f(m1, d1))
+#print(f(m2, d2))
+cha = f(m2, d2) - f(m1, d1)
+
+print(day[cha%7])
