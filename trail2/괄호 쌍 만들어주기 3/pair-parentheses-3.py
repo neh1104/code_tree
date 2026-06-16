97cc28c00d47bb7d86effae352656d9d0df1afda
노은호 <neh1104@hanyang.ac.kr>
2026-06-16 16:16:39 +0900

    [Passed] 괄호 쌍 만들어주기 3 (50ms, 16MB)

---
+++ trail2/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,12 @@
+A = input()
+
+# Please write your code here.
+n = len(A)
+cnt = 0
+for i in range(n-1):
+    if A[i] == '(':
+        for j in range(i+1, n):
+            if A[j] == ')':
+                cnt += 1
+
+print(cnt)
