20b125a796345fd3ffc3cfe3220fd538da715863
노은호 <neh1104@hanyang.ac.kr>
2026-06-16 16:02:15 +0900

    [Passed] 이동경로상에 있는 모든 숫자 더하기 (75ms, 18MB)

---
+++ trail2/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -0,0 +1,33 @@
+n, T = map(int, input().split())
+str = input()
+board = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+x = n//2; y = n//2
+sum = board[x][y]
+d = 0
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+def move(i):
+    global d, x, y
+    if i == 'L':
+        d = (d+3)%4
+        return 0
+    elif i == 'R':
+        d = (d+1)%4
+        return 0
+    else:
+        if in_range(x+dr[d], y+dc[d]):
+            x+=dr[d]; y+=dc[d]
+            return board[x][y]
+        else:
+            return 0
+
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+for i in str:
+    sum += move(i)
+
+print(sum)
+
