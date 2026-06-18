3bddb77207cd136623911b1f5373042f062553b0
노은호 <neh1104@hanyang.ac.kr>
2026-06-18 20:12:21 +0900

    [Passed] 숨은 단어 찾기 2 (53ms, 16MB)

--- trail2/숨은 단어 찾기 2/find-hidden-words-2.py
+++ trail2/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -1,17 +1,39 @@
-n, m = map(int, input().split())
-arr = [input() for _ in range(n)]
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+arr = [
+    input()
+    for _ in range(n)
+]
 
-# Please write your code here.
-dx = [0, 1, 1, 1, 0, -1, -1, -1]
-dy = [1, 1, 0, -1, -1, -1, 0, 1]
-cnt = 0
 def in_range(x, y):
-    return 0<=x<n and 0<=y<m
+    return 0 <= x and x < n and 0 <= y and y < m
+
+dxs, dys = [1, 1, 1, -1, -1, -1, 0, 0], [-1, 0, 1, -1, 0, 1, -1, 1]
+
+# 모든 좌표에서 다 확인해봅니다.
+cnt = 0
 for i in range(n):
+    # 격자를 벗어나지 않을 범위로만 잡습니다.
     for j in range(m):
-        if arr[i][j] == 'L':
-            for d in range(8):
-                if in_range(i+dx[d]*2, j+dy[d]*2):
-                    if arr[i+dx[d]][j+dy[d]] == 'E' and arr[i+dx[d]*2][j+dy[d]*2] == 'E':
-                        cnt += 1
+        
+        if arr[i][j] != 'L':
+            continue
+        
+        for dx, dy in zip(dxs, dys):
+            curt = 1
+            curx = i
+            cury = j
+            while True:
+                nx = curx + dx
+                ny = cury + dy
+                if in_range(nx, ny) == False:
+                    break
+                if arr[nx][ny] != 'E':
+                    break
+                curt += 1
+                curx = nx
+                cury = ny
+                if curt >= 3:
+                    cnt += 1
+                    break
 print(cnt)
