55531aae4441eadb527bbca205cc6865094d03d0
노은호 <neh1104@hanyang.ac.kr>
2026-07-01 17:37:03 +0900

    [Passed] 두 선분 (48ms, 16MB)

--- trail2/두 선분/two-lines.py
+++ trail2/두 선분/two-lines.py
@@ -2,7 +2,7 @@ x1, x2, x3, x4 = map(int, input().split())
 
 # Please write your code here.
 
-if (x1 <= x4 and x2>=x3) or (x2 >= x3 and x2 <= x4):
+if x1<=x4 and x2>=x3:
     print('intersecting')
 else:
     print('nonintersecting')
