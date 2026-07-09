4f2622b1d02678e115ebed1d3a3c43af9c0d7b8c
노은호 <neh1104@hanyang.ac.kr>
2026-07-09 19:31:33 +0900

    [Passed] L, R 그리고 B (52ms, 16MB)

---
+++ trail2/L, R 그리고 B/l-r-and-b.py
@@ -0,0 +1,22 @@
+board = [list(input()) for _ in range(10)]
+
+# Please write your code here.
+
+for i, bd in enumerate(board):
+    for b in range(len(bd)):
+        if bd[b] == 'L':
+            Lx, Ly = (i, b)
+        if bd[b] == 'B':
+            Bx, By = (i, b)
+        if bd[b] == 'R':
+            Rx, Ry = (i, b)
+x = abs(Lx-Bx)
+y = abs(Ly-By)
+
+if (Lx == Bx and Lx == Rx) or (Ly == By and Ly == Ry):
+    if (Lx-Rx)*(Bx-Rx) + (Ly-Ry)*(By-Ry) < 0:
+        print(x+y+1)
+    else:
+        print(x+y-1)
+else:
+    print(x+y-1)
