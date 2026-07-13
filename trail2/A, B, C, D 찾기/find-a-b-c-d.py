ab9521205467e4c804893fdd660a4294c4bdf425
노은호 <neh1104@hanyang.ac.kr>
2026-07-13 23:58:14 +0900

    [Passed] A, B, C, D 찾기 (49ms, 16MB)

---
+++ trail2/A, B, C, D 찾기/find-a-b-c-d.py
@@ -0,0 +1,17 @@
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+arr.sort()
+
+a = arr[0]
+b = arr[1]
+
+if arr[2] != a + b:
+    c = arr[2]
+else:
+    c = arr[3]
+
+d = arr[-1] - a - b - c
+
+print(a, b, c, d)
