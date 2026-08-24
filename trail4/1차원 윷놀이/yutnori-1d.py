48c71f5d6c22ad72ca0f07843b39a4b6ecaa2333
노은호 <neh1104@hanyang.ac.kr>
2026-08-25 00:56:41 +0900

    [Passed] 1차원 윷놀이 (6301ms, 24MB)

--- trail4/1차원 윷놀이/yutnori-1d.py
+++ trail4/1차원 윷놀이/yutnori-1d.py
@@ -1,41 +1,26 @@
-# 변수 선언 및 입력:
-n, m, k = tuple(map(int, input().split()))
+n, m, k = map(int, input().split())
 nums = list(map(int, input().split()))
-pieces = [1 for _ in range(k)]
 
-ans = 0
+# Please write your code here.
 
+lm = sum(nums) // (m-1) 
+k = k if k < lm else lm
 
-# 점수를 계산합니다.
-def calc():
-    score = 0
-    for piece in pieces:
-        score += (piece >= m)
-    
-    return score
+ls = [0 for _ in range(k)]
 
+MAX = 0
+def wornl(curr):
+    global MAX
+    global ls
 
-def find_max(cnt):
-    global ans
-    
-    # 말을 직접 n번 움직이지 않아도
-    # 최대가 될 수 있으므로 항상 답을 갱신합니다.
-    ans = max(ans, calc())
-    
-    # 더 이상 움직일 수 없으면 종료합니다.
-    if cnt == n: 
+    if curr == n:
+        MAX = max(MAX, len(list(i for i in ls if i >= m-1)))
         return
     
     for i in range(k):
-        # 움직여서 더 이득이 되지 않는
-        # 말은 더 이상 움직이지 않습니다.
-        if pieces[i] >= m:
-            continue
-        
-        pieces[i] += nums[cnt]
-        find_max(cnt + 1)
-        pieces[i] -= nums[cnt]
+        ls[i] += nums[curr]
+        wornl(curr+1)
+        ls[i] -= nums[curr]
 
-
-find_max(0)
-print(ans)
+wornl(0)
+print(MAX)
