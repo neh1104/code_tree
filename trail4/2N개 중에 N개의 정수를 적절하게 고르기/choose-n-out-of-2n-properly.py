45d84f8c9d892e7b50f0d9534f33a60ff3691aba
노은호 <neh1104@hanyang.ac.kr>
2026-08-26 18:14:00 +0900

    [Passed] 2N개 중에 N개의 정수를 적절하게 고르기 (197ms, 23MB)

--- trail4/2N개 중에 N개의 정수를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ trail4/2N개 중에 N개의 정수를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -1,42 +1,26 @@
-import sys
-
-INT_MAX = sys.maxsize
-
-# 변수 선언 및 입력:
-
 n = int(input())
 num = list(map(int, input().split()))
-visited = [False for _ in range(2 * n)]
 
-ans = INT_MAX
+# Please write your code here.
+s = sum(num)
+MIN = 999999
+def choose(curr, a, d):
+    global MIN
+    if d == n:
+        MIN = min(MIN, abs(a - abs(s-a)))
+        return
 
-
-def calc():
-    diff = 0
-    for i in range(2 * n):
-        diff = (diff + num[i]) if visited[i] else diff - num[i]
-    
-    return abs(diff)
-
-
-def find_min(idx, cnt):
-    global ans
-    
-    if cnt == n:
-        ans = min(ans, calc())
+    if curr == 2*n:
+        if d != n:
+            return
+        MIN = min(MIN, abs(a-abs(s-a)))
         return
     
-    if idx == 2 * n:
-        return
-    
-    # 현재 숫자를 첫 번째 그룹에 사용한 경우입니다.
-    visited[idx] = True
-    find_min(idx + 1, cnt + 1)
-    visited[idx] = False
-    
-    # 현재 숫자를 두 번째 그룹에 사용한 경우입니다.
-    find_min(idx + 1, cnt)
-    
+    k = num[curr]
+        
+    choose(curr+1, a+k, d+1)
 
-find_min(0, 0)
-print(ans)
+    choose(curr+1, a, d)
+    
+choose(0, 0, 0)
+print(MIN)
