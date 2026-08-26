d647156d4690c02923b13dab008a24dc693389a8
노은호 <neh1104@hanyang.ac.kr>
2026-08-26 23:08:37 +0900

    [Passed] 크기가 N인 순열 (191ms, 25MB)

--- trail4/크기가 N인 순열/n-permutation.py
+++ trail4/크기가 N인 순열/n-permutation.py
@@ -1,23 +1,24 @@
 n = int(input())
 
-visited = [False] * (n + 1)  # 숫자의 사용 여부 체크
-arr = []                     # 현재 만들어지고 있는 순열
+# Please write your code here.
+visited = [0 for i in range(n+1)]
+ls = []
+def choose(curr):
+    global ls
+    global visited
 
-def choose(curr_num):
-    # N개의 숫자를 모두 골랐다면 출력 후 종료
-    if curr_num == n + 1:
-        print(*arr)
+    if curr == n:
+        print(*ls)
         return
+    
+    for i in range(1, n+1):
+        if visited[i] == 1:
+            continue
+        visited[i] = 1
+        ls.append(i)
+        choose(curr+1)
+        visited[i] = 0
+        ls.pop()
 
-    # 1부터 N까지 순서대로 탐색 (사전순 출력을 보장)
-    for i in range(1, n + 1):
-        if not visited[i]:      # 아직 사용하지 않은 숫자라면
-            visited[i] = True   # 사용 처리
-            arr.append(i)
-            
-            choose(curr_num + 1) # 다음 위치의 숫자 정하러 재귀 호출
-            
-            arr.pop()           # 원상복구 (백트래킹)
-            visited[i] = False
 
-choose(1)
+choose(0)
