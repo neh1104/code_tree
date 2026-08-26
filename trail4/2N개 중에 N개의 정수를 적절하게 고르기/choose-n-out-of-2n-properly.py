4f1072fef588b1ca2c12f0d2a75488056ba1f433
노은호 <neh1104@hanyang.ac.kr>
2026-08-26 18:02:09 +0900

    [Passed] 2N개 중에 N개의 정수를 적절하게 고르기 (183ms, 24MB)

--- trail4/2N개 중에 N개의 정수를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ trail4/2N개 중에 N개의 정수를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -2,23 +2,24 @@ n = int(input())
 num = list(map(int, input().split()))
 
 # Please write your code here.
-
+s = sum(num)
 MIN = 999999
-def choose(curr, a, b, d):
+def choose(curr, a, d):
     global MIN
+    if d == n:
+        MIN = min(MIN, abs(a - abs(s-a)))
+        return
+
     if curr == 2*n:
         if d != n:
             return
-        #print(a, b)
-        MIN = min(MIN, abs(a-b))
+        MIN = min(MIN, abs(a-abs(s-a)))
         return
     
     k = num[curr]
-    if d == n:
-        choose(curr+1, a, b+k, d)
-    else:
-        choose(curr+1, a+k, b, d+1)
+        
+    choose(curr+1, a+k, d+1)
 
-        choose(curr+1, a, b+k, d)
-choose(0, 0, 0, 0)
+    choose(curr+1, a, d)
+choose(0, 0, 0)
 print(MIN)
