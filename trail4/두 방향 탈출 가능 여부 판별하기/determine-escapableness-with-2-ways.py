924ba985ad4d1e6ab4852f306aa9192cef4df8b5
노은호 <neh1104@hanyang.ac.kr>
2026-08-27 16:14:08 +0900

    [Passed] 두 방향 탈출 가능 여부 판별하기 (51ms, 16MB)

---
+++ trail4/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+ch = 0
+vt = [[1 for _ in range(n)] for _ in range(m)]
+def can_go(x, y):
+    return 0<=x<n and 0<=y<m and grid[x][y] != 0
+
+dr = [1, 0]
+dc = [0, 1]
+def dfs(r, c):
+    global ch
+    if ch:
+        return
+
+    if r == n-1 and c == m-1:
+        ch = 1
+        return
+    
+    grid[r][c] = 0
+
+    for dx, dy in zip(dr, dc):
+        x = r+dx; y = c+dy
+        if can_go(x, y):
+            dfs(x, y)
+
+dfs(0, 0)
+print(ch)
