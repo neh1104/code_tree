5405b12c769a53671325895937eaa67e4bd854a5
노은호 <neh1104@hanyang.ac.kr>
2026-08-27 13:38:36 +0900

    [Passed] 외판원 순회 (181ms, 20MB)

--- trail4/외판원 순회/traveling-salesman-problem.py
+++ trail4/외판원 순회/traveling-salesman-problem.py
@@ -3,29 +3,30 @@ A = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 import sys
-visited = [False] * n
+vt = [0 for i in range(n)]
 MIN = sys.maxsize
 
-def choose(curr, cost, cnt):
+def choose(curr, d, cnt):
+    global vt
     global MIN
 
-    if cost >= MIN:
+    if d >= MIN:
         return
 
     if cnt == n:
-        if A[curr][0] != 0:
-            MIN = min(MIN, cost + A[curr][0])
+        if A[curr][0] == 0:
+            return
+        MIN = min(MIN, d+A[curr][0])
         return
 
-    for nxt in range(n):
-        if visited[nxt] or A[curr][nxt] == 0:
+    for i in range(n):
+        if A[curr][i] == 0 or vt[i] == 1:
             continue
 
-        visited[nxt] = True
-        choose(nxt, cost + A[curr][nxt], cnt + 1)
-        visited[nxt] = False
+        vt[i] = 1
+        choose(i, d+A[curr][i], cnt+1)
+        vt[i] = 0
 
-visited[0] = True
+vt[0] = 1
 choose(0, 0, 1)
-
 print(MIN)
