d49d322e7e6aeeb29bc4ae9789a40fbe253a8b7e
노은호 <neh1104@hanyang.ac.kr>
2026-08-27 13:26:37 +0900

    [Passed] 외판원 순회 (252ms, 21MB)

--- trail4/외판원 순회/traveling-salesman-problem.py
+++ trail4/외판원 순회/traveling-salesman-problem.py
@@ -3,44 +3,37 @@ A = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 import sys
-vt = [-1 for i in range(n)]
+vt = [0 for i in range(n)]
 ls = []; MIN = sys.maxsize
-
-def cycle(vt):
-    x = 0; cnt = 0
-    while x != -1:
-        x = vt[x]
-        cnt += 1
-        if cnt == n:
-            break
-        if x == 0:
-            return True
-    return False
-
-def choose(curr, d):
+c = [0 for _ in range(n)]
+def choose(curr, d, cnt):
     global vt
     global ls
     global MIN
-    
+    global c
+
     if d >= MIN:
         return
 
-    if cycle(vt):
+    if cnt == n:
+        #print(ls, d, vt)
+        #print(ls)
+        MIN = min(MIN, d+A[curr][0])
         return
-
-    if curr == n:
-        #print(ls, sum(ls), vt)
-        MIN = min(MIN, sum(ls))
+    if c[curr] == 1:
         return
-    
+    c[curr] = 1
 
     for i in range(n):
-        if A[curr][i] == 0 or vt[i] != -1:
+        if A[curr][i] == 0 or vt[i] != 0:
             continue
 
-        vt[i] = curr; ls.append(A[curr][i])
-        choose(curr+1, d+A[curr][i])
-        vt[i] = -1; ls.pop()
+        vt[i] = 1; ls.append(A[curr][i])
+        #print(curr, i)
+        #print(ls)
+        choose(i, d+A[curr][i], cnt+1)
+        vt[i] = 0; ls.pop()
+    c[curr] = 0
 
-choose(0, 0)
+choose(0, 0, 0)
 print(MIN)
