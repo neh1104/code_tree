ffc30276134f09f656286b5800051fbcbd498165
노은호 <neh1104@hanyang.ac.kr>
2026-08-28 22:52:24 +0900

    [Passed] 빙하 (55ms, 16MB)

--- trail4/빙하/glacier.py
+++ trail4/빙하/glacier.py
@@ -1,54 +1,53 @@
+from collections import deque
+
 n, m = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
-
-# Please write your code here.
-from collections import deque
 
 dr = [1, 0, -1, 0]
 dc = [0, 1, 0, -1]
 
-vt = [[0 for _ in range(m)] for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 
-def in_range(x, y):
-    return 0<=x<n and 0<=y<m and vt[x][y] == 0
+q = deque([(0, 0)])
+visited[0][0] = True
 
-def bfs():
-    global cnt
-    global how_many
+time = 0
+last = 0
+
+while q:
+    next_q = deque()
+    melted = 0
+
     while q:
         r, c = q.popleft()
-        #print(r, c)
+
         for d in range(4):
-            x = r+dr[d]; y = c+dc[d]
-            if in_range(x, y):
-                if nvt[x][y]:
-                    continue
-                if a[x][y]:
-                    nq.append((x, y))
-                    nvt[x][y] = 1
-                    how_many += 1
-                else:
-                    #print(x,y)
-                    q.append((x, y))
-                    vt[x][y] = 1
-                    cnt += 1
-                    
+            nr = r + dr[d]
+            nc = c + dc[d]
 
+            if not (0 <= nr < n and 0 <= nc < m):
+                continue
 
-cnt = 0; nq = deque([(0, 0)])
-time = -1
-ls = []
-while cnt < n*m:
-    q = deque(nq); nq = deque()
-    if q == deque([]):
+            if visited[nr][nc]:
+                continue
+
+            visited[nr][nc] = True
+
+            if a[nr][nc] == 1:
+                next_q.append((nr, nc))
+                melted += 1
+            else:
+                q.append((nr, nc))
+
+    if not next_q:
         break
-    r, c = q[0]; vt[r][c] = 1; cnt += 1
-    nvt = [[0 for _ in range(m)] for _ in range(n)]
-    how_many = 0; time += 1
 
-    bfs()
-    ls.append(how_many)
-    #print(q, nq)
-    for i, j in nq:
-        a[i][j] = 0
-print(time, ls[-2])
+    last = melted
+    time += 1
+
+    for r, c in next_q:
+        a[r][c] = 0
+
+    q = next_q
+
+print(time, last)
