ab99a21c1f95ba7841aef794d9a9d0c23e1ac005
노은호 <neh1104@hanyang.ac.kr>
2026-08-30 00:31:53 +0900

    [Passed] 정수 사각형 최장 증가 수열 (401ms, 79MB)

---
+++ trail4/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,31 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+dr = [1, 0, -1, 0]
+dc = [0, 1, 0, -1]
+
+def dp(r, c):
+    if memo[r][c] != -1:
+        return memo[r][c]
+    now = grid[r][c]
+
+    MAX = 1
+    for d in range(4):
+        x = r + dr[d]
+        y = c + dc[d]
+        if 0 <= x < n and 0 <= y < n and grid[x][y] > now:
+            MAX = max(MAX, dp(x, y) + 1)
+        memo[r][c] = MAX
+    return memo[r][c]
+
+MAX = 0
+memo = [[-1 for _ in range(n)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):        
+        MAX = max(MAX, dp(i, j))
+        #print(*memo, sep = '\n')
+        #print()
+print(MAX)
