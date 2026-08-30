4bb73678f3ff8e7e5ea931a38da2e3543a88f4ee
노은호 <neh1104@hanyang.ac.kr>
2026-08-30 18:04:05 +0900

    [Passed] 알바로 부자 되기 (48ms, 16MB)

---
+++ trail4/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -0,0 +1,24 @@
+n = int(input())
+jobs = [tuple(map(int, input().split())) for _ in range(n)]
+s = [job[0] for job in jobs]
+e = [job[1] for job in jobs]
+p = [job[2] for job in jobs]
+
+# Please write your code here.
+#top_down
+vt = [0 for _ in range(n)]
+
+def dp(curr):
+    if vt[curr] != 0:
+        return vt[curr]
+    price = p[curr]
+    vt[curr] = price
+    for i in range(curr+1, n):
+        I = dp(i)
+        if s[i] > e[curr]:
+            vt[curr] = max(vt[curr], I+price) 
+    return vt[curr]
+
+dp(0)
+#print(vt)
+print(max(vt))
