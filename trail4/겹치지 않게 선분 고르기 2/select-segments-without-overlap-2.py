bdd37c79d85c0e81173299c51f54d1101aeb7295
노은호 <neh1104@hanyang.ac.kr>
2026-08-30 16:20:21 +0900

    [Passed] 겹치지 않게 선분 고르기 2 (67ms, 17MB)

---
+++ trail4/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -0,0 +1,23 @@
+n = int(input())
+x1, x2 = [], []
+lines = []
+for _ in range(n):
+    a, b = map(int, input().split())
+    lines.append((a, b))
+
+# Please write your code here.
+
+vt = [1 for _ in range(n)]
+lines.sort(key = lambda x :(x[0], x[1]))
+
+#bottom-top
+
+for i in range(n):
+    a, b = lines[i]
+    for j in range(i):
+        x1, x2 = lines[j]
+        if x2 >= a and x1 <= b:
+            continue
+        vt[i] = max(vt[i], vt[j]+1)
+
+print(max(vt))
