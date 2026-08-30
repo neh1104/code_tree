67916d3548d1f613de8e4c8f1ee91db8f1eb7fdc
노은호 <neh1104@hanyang.ac.kr>
2026-08-31 00:12:11 +0900

    [Passed] 계단 오르기 2 (50ms, 16MB)

--- trail4/계단 오르기 2/climbing-stairs-2.py
+++ trail4/계단 오르기 2/climbing-stairs-2.py
@@ -11,8 +11,6 @@ vt[1][1] = coin[1]
 for i in range(2, n+1):
     vt[i][0] = vt[i-2][0]+coin[i]
     for j in range(1, 4):
-        if vt[i-1][j-1] == INT_MIN and vt[i-2][j] == INT_MIN:
-            continue
         vt[i][j] = max(vt[i-1][j-1], vt[i-2][j]) + coin[i]
 
 print(max(vt[n]))
