4ff5e83653c75185477e3785487a70762f0e2a9e
노은호 <neh1104@hanyang.ac.kr>
2026-09-01 23:19:00 +0900

    [Passed] 369 게임 (48ms, 16MB)

--- trail1/369 게임/369-game.py
+++ trail1/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1, n+1):
-    if set(str(i)) & set('369') or i % 3 == 0: #there are more CLEAN statement!
+    if set(str(i)) & set('369') or i % 3 == 0: #there is a CLEANER statement!
         print(0, end = ' ')
     else:
         print(i, end = ' ')
