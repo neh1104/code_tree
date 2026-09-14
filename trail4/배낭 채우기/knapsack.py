7af7cfd7c0ebd4eb18bcc1d63b1e7ee351990588
노은호 <neh1104@hanyang.ac.kr>
2026-09-14 21:10:30 +0900

    [Passed] 배낭 채우기 (49ms, 16MB)

---
+++ trail4/배낭 채우기/knapsack.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+w, v = zip(*[tuple(map(int, input().split())) for _ in range(N)])
+w, v = list(w), list(v)
+
+# Please write your code here.
+
+dp = [0] * (M + 1)
+
+for i in range(N):
+    for j in range(M, w[i] - 1, -1):
+        dp[j] = max(dp[j], dp[j - w[i]] + v[i])
+
+print(max(dp))
