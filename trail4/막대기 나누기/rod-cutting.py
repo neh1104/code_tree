5d3802d7246dd8e4f485b8da67a79ea1e41e91a3
노은호 <neh1104@hanyang.ac.kr>
2026-09-16 22:51:31 +0900

    [Passed] 막대기 나누기 (50ms, 16MB)

--- trail4/막대기 나누기/rod-cutting.py
+++ trail4/막대기 나누기/rod-cutting.py
@@ -3,10 +3,13 @@ profit = list(map(int, input().split()))
 
 # Please write your code here.
 
-dp = [0] * (n + 1)
-
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        dp[i] = max(dp[i], dp[i - j] + profit[j - 1])
-
+dp = [-1]*(n+1)
+dp[0] = 0
+for i in range(n):
+    if dp[i] == -1:
+        continue
+    for j in range(n):
+        if i+j+1 > n:
+            continue
+        dp[i+j+1] = max(dp[i+j+1], dp[i]+profit[j])
 print(dp[n])
