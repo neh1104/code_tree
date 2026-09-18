eacc87ffdfb1a4c8123c6c56043ca332eef986ae
노은호 <neh1104@hanyang.ac.kr>
2026-09-18 17:46:27 +0900

    [Passed] 동일하게 분할 (175ms, 32MB)

--- trail4/동일하게 분할/equal-partition.py
+++ trail4/동일하게 분할/equal-partition.py
@@ -5,21 +5,19 @@ arr = list(map(int, input().split()))
 
 s = sum(arr)
 if s%2 == 0:
-    dp = [-1]*(2*s+1)
-    dp[s] = 0
+    dp = [0]*(2*s+1)
+    dp[s] = 1
     for x in arr:
-        ndp = [-1]*(2*s+1)
+        ndp = [0]*(2*s+1)
         for i in range(2*s+1):
-            if dp[i] == -1:
-                continue
-
-            if i+x < 2*s+1:
-                ndp[i+x] = max(ndp[i+x], dp[i]+x)
-            if i-x >= 0:
-                ndp[i-x] = max(ndp[i-x], dp[i]+x)
+            if dp[i]:
+                if i+x < 2*s+1:
+                    ndp[i+x] = 1
+                if i-x >= 0:
+                    ndp[i-x] = 1
         dp = ndp
-    #print(dp[s], dp)
-    print('Yes' if dp[s] != -1 else 'No')
+
+    print('Yes' if dp[s] else 'No')
 
 else:
     print('No')
