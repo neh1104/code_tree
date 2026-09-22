72d45b74f014982dcd1c2fc16b7c3b5ce13d5110
노은호 <neh1104@hanyang.ac.kr>
2026-09-22 14:06:52 +0900

    [Passed] 게임 스튜디오에서 살아남기 (90ms, 19MB)

---
+++ trail4/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+# Please write your code here.
+
+dp = [[[0 for _ in range(3)] for _ in range(n+1)] for _ in range(4)]
+dp[0][0][0] = 1
+for t in range(3):
+    for i in range(1, n+1):
+        s = sum(dp[t][i-1][:])
+        dp[t][i][0] += s
+        dp[t+1][i][0] += s 
+        for j in range(1, 3):
+            dp[t][i][j] = dp[t][i-1][j-1]
+
+SUM = sum([sum(arr[n]) for arr in dp[:3]])
+print(SUM%(10**9+7))
