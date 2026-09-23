186710f9bbd7ebe332606839a66fd1aea28419ab
노은호 <neh1104@hanyang.ac.kr>
2026-09-23 19:45:03 +0900

    [Passed] 축구와 야구 (105ms, 20MB)

--- trail4/축구와 야구/soccer-and-baseball.py
+++ trail4/축구와 야구/soccer-and-baseball.py
@@ -32,36 +32,6 @@ for i in range(1, n+1):
                     dp[i][j][k],
                     dp[i-1][j][k-1] + b[i-1]
                 )
-            dp[i][j][k] = dp[i-1][j][k]
-
-            # 축구팀에 넣는 경우
-            if j > 0 and dp[i-1][j-1][k] != INT_MIN:
-                dp[i][j][k] = max(
-                    dp[i][j][k],
-                    dp[i-1][j-1][k] + s[i-1]
-                )
-
-            # 야구팀에 넣는 경우
-            if k > 0 and dp[i-1][j][k-1] != INT_MIN:
-                dp[i][j][k] = max(
-                    dp[i][j][k],
-                    dp[i-1][j][k-1] + b[i-1]
-                )
-            dp[i][j][k] = dp[i-1][j][k]
-
-            # 축구팀에 넣는 경우
-            if j > 0 and dp[i-1][j-1][k] != INT_MIN:
-                dp[i][j][k] = max(
-                    dp[i][j][k],
-                    dp[i-1][j-1][k] + s[i-1]
-                )
-
-            # 야구팀에 넣는 경우
-            if k > 0 and dp[i-1][j][k-1] != INT_MIN:
-                dp[i][j][k] = max(
-                    dp[i][j][k],
-                    dp[i-1][j][k-1] + b[i-1]
-                )
 
 print(dp[n][11][9])
 #print(*dp, sep = '\n')
