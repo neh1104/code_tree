a73cd6605f591d3aba49b2c4f843c18e0080a339
노은호 <neh1104@hanyang.ac.kr>
2026-09-23 19:46:41 +0900

    [Passed] 축구와 야구 (103ms, 20MB)

--- trail4/축구와 야구/soccer-and-baseball.py
+++ trail4/축구와 야구/soccer-and-baseball.py
@@ -10,28 +10,16 @@ for _ in range(n):
 import sys
 INT_MIN = -sys.maxsize
 
-dp = [[[INT_MIN]*10 for _ in range(12)] for _ in range(n+1)]
+dp = [[[INT_MIN]*11 for _ in range(13)] for _ in range(n+1)]
 for i in range(n+1):
     dp[i][0][0] = 0
 
 for i in range(1, n+1):
     for j in range(12):
         for k in range(10):
-            dp[i][j][k] = dp[i-1][j][k]
-
-            # 축구팀에 넣는 경우
-            if j > 0 and dp[i-1][j-1][k] != INT_MIN:
-                dp[i][j][k] = max(
-                    dp[i][j][k],
-                    dp[i-1][j-1][k] + s[i-1]
-                )
-
-            # 야구팀에 넣는 경우
-            if k > 0 and dp[i-1][j][k-1] != INT_MIN:
-                dp[i][j][k] = max(
-                    dp[i][j][k],
-                    dp[i-1][j][k-1] + b[i-1]
-                )
+            S = -1 if dp[i-1][j-1][k] == INT_MIN else s[i-1]
+            B = -1 if dp[i-1][j][k-1] == INT_MIN else b[i-1]
+            dp[i][j][k] = max(dp[i-1][j-1][k]+S, dp[i-1][j][k-1]+B, dp[i-1][j][k])
 
 print(dp[n][11][9])
 #print(*dp, sep = '\n')
